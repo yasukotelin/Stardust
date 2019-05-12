2f645f64aa8e2623e677063520c3a74cf5bdd2db
yasukotelin <yasukotelin@gmail.com>
2019-05-13 01:27:57 +0900

    Add the function which register the applications

--- Stardust/MainWindow.xaml.cs
+++ Stardust/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,41 @@ namespace Stardust
         {
             InitializeComponent();
         }
+
+        private void OnClickFilePickerButton(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            var isSelected = dialog.ShowDialog();
+
+            if (isSelected == true)
+            {
+                PathTextBox.Text = dialog.FileName;
+            }
+            else
+            {
+                PathTextBox.Text =
+                    Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+            }
+        }
+
+        private void OnClickRegisterButton(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var startMenuRegister = new StartMenuRegister(PathTextBox.Text);
+                startMenuRegister.Register();
+            }
+            catch (FileNotFoundException ex)
+            {
+                ShowErrorDialog(ex);
+            }
+            catch (ShortcutAlreadyExistException ex)
+            {
+                ShowErrorDialog(ex);
+            }
+        }
+
+        private void ShowErrorDialog(Exception e)
+            => MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
     }
 }
---
+++ Stardust/StartMenuRegister.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stardust
+{
+    class ShortcutAlreadyExistException : Exception
+    {
+        public ShortcutAlreadyExistException(string message) : base(message)
+        {
+        }
+    }
+
+
+    class StartMenuRegister
+    {
+        private readonly string directoryPlacedShortcut =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Stardust");
+
+        private string ApplicationPath { get; set; }
+
+
+        private string ShorcutLinkFileName
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(ApplicationPath) + ".lnk";
+            }
+        }
+
+        private string ShortcutPath
+        {
+            get
+            {
+                return Path.Combine(directoryPlacedShortcut, ShorcutLinkFileName);
+            }
+        }
+
+        public StartMenuRegister(string applicationPath)
+        {
+            ApplicationPath = applicationPath;
+
+            if (!File.Exists(ApplicationPath))
+                throw new FileNotFoundException($"Application path {ApplicationPath} is not exist.");
+
+            if (File.Exists(ShortcutPath))
+                throw new ShortcutAlreadyExistException($"Shortcut {ShortcutPath} is already exist");
+        }
+
+        /// <summary>
+        /// Register the application to start menu.
+        /// </summary>
+        public void Register()
+        {
+            CreateIfDirectoryNotExist();
+            CreateShortcut();
+        }
+
+        private void CreateShortcut()
+        {
+            var shell = new IWshRuntimeLibrary.WshShell();
+            var shorcutObj = (IWshRuntimeLibrary.IWshShortcut) shell.CreateShortcut(ShortcutPath);
+            shorcutObj.TargetPath = ApplicationPath;
+            shorcutObj.Save();
+
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(shorcutObj);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(shell);
+        }
+
+        private void CreateIfDirectoryNotExist()
+        {
+            Directory.CreateDirectory(directoryPlacedShortcut);
+        }
+    }
+}
